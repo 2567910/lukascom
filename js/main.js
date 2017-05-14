612d8c4a7b7e6c844dc2bd23fc61c80e55f8d5da
2567910 <luki.seyfarth@web.de>
2017-05-14 20:30:03 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,48 +1,5 @@
 // Main Javascript
 //Fading elements
-$(document).ready(function() {
-
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-
-                $(this).animate({'opacity':'1'},3500);
-
-            }
-
-        });
-
-    });
-
-});
-
-//load more button
-$(function(){
-    $("div").slice(0, 20).show(); // select the first ten
-    $("#loadm").click(function(e){ // click event for load more
-        e.preventDefault();
-        $("div:hidden").slice(0, 4).show(); // select next 10 hidden divs and show them
-        if($("div:hidden").length == 0){ // check if any hidden divs still exist
-            alert("No more divs"); // alert if there are none left
-        }
-    });
-});
-//hide the button after
-    var button = document.getElementById('loadm')
-    button.addEventListener('click',hideshow,false);
-
-    function hideshow() {
-        document.getElementById('hidden-div').style.display = 'block';
-        this.style.display = 'none'
-    }
 
 (function($) {
     "use strict"; // Start of use strict
