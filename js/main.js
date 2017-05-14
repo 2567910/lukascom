2d5169a5fd3867c180f29c4d4b563a2411aab532
2567910 <luki.seyfarth@web.de>
2017-05-14 20:31:50 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-// Main Javascript
-//Fading elements
 
+// Main Javascript
 (function($) {
     "use strict"; // Start of use strict
 
@@ -43,3 +42,28 @@
     });
 
 })(jQuery); // End of use strict
+
+//Fading elements
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},3500);
+
+            }
+
+        });
+
+    });
+
+});
